6fca3cb6e62129c6fb5cfe3c629dc11d530f20c4
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-11-30 09:35:45 +0700

    Setting theme & gradient background main activity

--- app/src/main/java/com/esd/expeditioninfo/MainActivity.java
+++ app/src/main/java/com/esd/expeditioninfo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.esd.expeditioninfo;
 
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+        window.getDecorView().getBackground().setDither(true);
     }
 }
