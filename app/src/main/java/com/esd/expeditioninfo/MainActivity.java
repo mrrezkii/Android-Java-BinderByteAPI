d53db9887add2b0745632c92965f3034dd1053e3
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-11-30 14:08:24 +0700

    Configure fragment

--- app/src/main/java/com/esd/expeditioninfo/MainActivity.java
+++ app/src/main/java/com/esd/expeditioninfo/MainActivity.java
@@ -2,12 +2,25 @@ package com.esd.expeditioninfo;
 
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+import com.esd.expeditioninfo.Fragment.PostageFragment;
+import com.esd.expeditioninfo.Fragment.TrackingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
+
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        loadFragment(new PostageFragment());
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -25,5 +47,29 @@ public class MainActivity extends AppCompatActivity {
         Window window = getWindow();
         window.setFormat(PixelFormat.RGBA_8888);
         window.getDecorView().getBackground().setDither(true);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.menuPostage:
+                fragment = new PostageFragment();
+                break;
+            case R.id.menuTracking:
+                fragment = new TrackingFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
